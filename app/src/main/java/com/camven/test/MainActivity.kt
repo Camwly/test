ab72411d88836f5642e21b418bc90c2b3583d68e
camvenli <990828754@qq.com>
2023-02-07 15:30:13 +0800

    feature:commit 1

--- app/src/main/java/com/camven/test/MainActivity.kt
+++ app/src/main/java/com/camven/test/MainActivity.kt
@@ -2,10 +2,16 @@ package com.camven.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test()
     }
+}
+
+fun test(){
+    Log.i("camvenli","This is commit 1")
 }
